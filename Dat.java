08c10d8525a6ee1d603cfb9194eb512251fbe739
DatNguyen51600012 <43365714+DatNguyen51600012@users.noreply.github.com>
2018-09-25 09:07:02 +0700

    Update Dat.java

--- Dat.java
+++ Dat.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
 public class HelloWorld {
 
-public static void main(String[] args) {
-    // Prints "Hello, World" to the terminal window.
-    System.out.println("Total Dat Students");
- }
+	public static void main(String[] args) {
+		
+		static String filename = "input file : ";
+    		public static void main(String[] args) {
+		System.out.println("Hello world I'm TDTU");
+		int count= 0;
+                
+		try{
+		    Scanner sc = new Scanner(new FileReader(new File(filename)) );
+		    while (sc.hasNext()){
+			String line = sc.next();
+			System.out.println("Helo world, I'm " + line);
+			count++;
+		    }
+
+            
+		} catch(IOException e){
+
+		}
+		System.out.println("Total " + count + " students");
+    }
+	}
+
 }
